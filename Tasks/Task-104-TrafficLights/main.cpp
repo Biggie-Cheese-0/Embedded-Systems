b6751b633186dcb165cdd6c6e614eaff14237403
Biggie-Cheese-0 <146337854+Biggie-Cheese-0@users.noreply.github.com>
2023-10-05 15:28:20 +0100

    Lab2.2

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -3,7 +3,7 @@ using namespace uop_msb;
 
 #define WAIT_TIME_MS 500 
 
-DigitalOut red(TRAF_RED1_PIN,1);         //Note the initial state
+DigitalOut red(TRAF_RED1_PIN,0);         //Note the initial state
 DigitalOut amber(TRAF_YEL1_PIN,0);
 DigitalOut green(TRAF_GRN1_PIN,0);
 
@@ -11,27 +11,37 @@ LCD_16X2_DISPLAY lcd;
 
 int main()
 {
-    lcd.puts("RED");
-    wait_us(1000000);
-
-    amber = 1;
-    lcd.cls();
-    lcd.puts("Amber");
-    wait_us(1000000);
-
-    green = 1;
-    lcd.cls();
-    lcd.puts("Green");    
-    wait_us(1000000);
-
-    lcd.cls();
-    lcd.puts("TASK-104");
-
-    while (true)
+        lcd.cls();
+        lcd.puts("TASK-104");
+        wait_us(200000);
+        while (true)
     {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
+        
+    
+        
+        wait_us(WAIT_TIME_MS * 10000);
+        
+        lcd.cls();
+        lcd.puts("Red");
+        red = 1;
+        wait_us(10000000);
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("Red and Amber");
+        wait_us(2000000);
+
+        red = 0;
+        amber = 0;
+
+        green = 1;
+        lcd.cls();
+        lcd.puts("Green");    
+        wait_us(10000000);
+
+        green = 0;
+        amber = 1;
+        wait_us(2000000);
+        amber = 0;
     }
 }
